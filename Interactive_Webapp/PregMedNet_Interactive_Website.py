5e5bedc3aa4430c4a7820e9c95caf06ed7347959
Yera Kim <ykim824@anes-nalab2.stanford.edu>
2024-06-04 16:51:00 -0700

    test directory

--- Interactive_Webapp/PregMedNet_Interactive_Website.py
+++ Interactive_Webapp/PregMedNet_Interactive_Website.py
@@ -269,8 +269,8 @@ with tab3:
         # file_path_ddi_edge = Path(__file__).parents[0] / '2024_reference_tables/drug-drug-interactions/ddi_edge_both_cohort_df.csv'
         file_path_df1 =  Path(__file__).parents[0] / '2024_reference_tables/final_mother_baby_dyads_data/FINAL_MOM_BABY_DATA_DF1.csv'
         file_path_df2 =  Path(__file__).parents[0] / '2024_reference_tables/final_mother_baby_dyads_data/FINAL_MOM_BABY_DATA_DF2.csv'
-        df1 = pd.read_csv(file_path_df1).set_index('ENROLID_BABY')
-        df2 = pd.read_csv(file_path_df2).set_index('ENROLID_BABY')
+        df1 = pd.read_csv(file_path_df1)#.set_index('ENROLID_BABY')
+        df2 = pd.read_csv(file_path_df2)#.set_index('ENROLID_BABY')
         df = pd.concat([df1,df2])
         print('file loaded!')
 
