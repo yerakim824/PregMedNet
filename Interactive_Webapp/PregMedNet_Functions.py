101f5b8d1964e56951901cdd33f6dab72cd80c06
Yera Kim <ykim824@anes-nalab2>
2024-11-12 08:32:02 +0000

    changed the directory

--- Interactive_Webapp/PregMedNet_Functions.py
+++ Interactive_Webapp/PregMedNet_Functions.py
@@ -76,8 +76,8 @@ def RAW_ODDS_ALL():
     return raw_or_all
 
 def ADJ_ODDS_ALL():
-    file_path_raw_or = Path(__file__).parents[0] / '2024_reference_tables/adj_or_all_nov2024.csv'
-    adj_or_all = pd.read_csv(file_path_raw_or).drop(columns=['Unnamed: 0'],axis=1)
+    file_path_adj_or = Path(__file__).parents[0] / '2024_reference_tables/adj_or_all_nov2024.csv'
+    adj_or_all = pd.read_csv(file_path_adj_or).drop(columns=['Unnamed: 0'],axis=1)
     return adj_or_all
 
 
