64aa4ae74c3f0d863846109a7c9775707252b834
Yera Kim <ykim824@stanford.edu>
2025-03-14 05:34:07 +0000

    removed unnecessary testing files

--- Interactive_Webapp/auto_empty_commit.sh
+++
@@ -1,7 +0,0 @@
-git commit --allow-empty -m "automate empty commit: $(date)"
-git push origin main
-
-# The job is automated with crontab to run every day at midnight. Check the status  by
-# crontab -l
-# OR
-# crontab -e
